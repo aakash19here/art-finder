bc27ec92eb68d4ac5750fca5861d0919ee6989d8
Aakash <aakash19here@gmail.com>
2025-01-19 03:44:45 +0530

    feat: add scraping

---
+++ lib/firecrawl.ts
@@ -0,0 +1,3 @@
+import FirecrawlApp from "@mendable/firecrawl-js";
+
+export const app = new FirecrawlApp({ apiKey: process.env.FIRECRAWL_API_KEY });
---
+++ lib/tools/firecrawl-scrape.ts
@@ -0,0 +1,39 @@
+import { app } from "../firecrawl";
+
+export type ScrapeResult = {
+  markdown: string;
+};
+
+export async function scrape(urls: string[]) {
+  const scrapeResults: ScrapeResult[] = [];
+  const sourceUrls: string[] = [];
+  const sourceTitles: string[] = [];
+
+  const metadata = {
+    sourceUrls,
+    sourceTitles,
+  };
+
+  for (const url of urls) {
+    const result = await app.scrapeUrl(url, {
+      formats: ["markdown"],
+    });
+    if (result.success) {
+      scrapeResults.push({
+        markdown: result.markdown ?? "",
+      });
+
+      sourceUrls.push(url);
+      sourceTitles.push(result.title ?? "");
+    } else {
+      console.error("Error scraping URL", url, result.error);
+    }
+  }
+
+  const markdown = scrapeResults.map((result) => result.markdown).join("\n");
+
+  return {
+    markdown,
+    metadata,
+  };
+}
